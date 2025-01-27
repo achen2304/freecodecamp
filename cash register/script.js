8f21fda10c1e8c317b12ae937000c7219fdd0de2
Andy Chen <achen2304@gmail.com>
2025-01-27 15:43:24 -0600

    begin script for cash reg

--- cash register/script.js
+++ cash register/script.js
@@ -10,3 +10,36 @@ let cid = [
   ["TWENTY", 60],
   ["ONE HUNDRED", 100],
 ];
+
+const inputCash = document.getElementById("cash");
+const output = document.getElementById("change-due");
+const purchaseBtn = document.getElementById("purchase-btn");
+const cidTotal = 0;
+
+purchaseBtn.addEventListener("click", () => {
+  const cash = parseFloat(inputCash.value);
+  const change = checkCashRegister(price, cash, cid);
+  output.innerHTML = change;
+});
+
+function checkCashRegister(price, cash, cid) {
+  if (price > cash) {
+    alert("Customer does not have enough money to purchase the item");
+    return;
+  }
+  if (price === cash) {
+    output.innerHTML = "No change due - customer paid with exact cash";
+    return;
+  }
+  console.log(calcualteCidTotal(cid).toFixed(2));
+  if (calcualteCidTotal(cid).toFixed(2) < cash - price) {
+    output.innerHTML = "Status: INSUFFICIENT_FUNDS";
+    return;
+  }
+}
+
+const calcualteCidTotal = (cid) => {
+  return cid.reduce((acc, curr) => {
+    return acc + curr[1];
+  }, 0);
+};
