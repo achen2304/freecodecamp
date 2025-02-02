fb683be738e36a531baa1604e2729bdb4999b0a7
Andy <achen2304@gmail.com>
2025-02-02 00:24:19 -0600

    finished backend/api cert

--- BackEndDevAPIs/url-shortner/urlshortener/index.js
+++ BackEndDevAPIs/url-shortner/urlshortener/index.js
@@ -7,10 +7,9 @@ const dns = require('dns');
 const urlparser = require('url');
 const { error } = require('console');
 
-const client = new MongoClient(process.env.MONGO_URL)
-const db = client.db("urlshortner");
-const urls = db.collection("urls");
-
+const client = new MongoClient(process.env.MONGO_URL);
+const db = client.db('urlshortner');
+const urls = db.collection('urls');
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -18,43 +17,46 @@ const port = process.env.PORT || 3000;
 //middleware
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
 // Your first API endpoint
-app.post('/api/shorturl', function(req, res) {
-  console.log(req.body)
-  const url = req.body.url
-  const dnsLookup = dns.lookup(urlparser.parse(url).hostname, async (error, address) => {
-    if(!address) return res.json({error: "Invalid URL"});
-    const count = await urls.countDocuments({});
-    const urlDoc = {
-      url: url,
-      short_url: count
-    }
+app.post('/api/shorturl', function (req, res) {
+  console.log(req.body);
+  const url = req.body.url;
+  const dnsLookup = dns.lookup(
+    urlparser.parse(url).hostname,
+    async (error, address) => {
+      if (!address) return res.json({ error: 'Invalid URL' });
+      const count = await urls.countDocuments({});
+      const urlDoc = {
+        url: url,
+        short_url: count,
+      };
 
-    const result = await urls.insertOne(urlDoc);
-    console.log(result);
-    res.json({ original_url: url, short_url: count})
-  })
+      const result = await urls.insertOne(urlDoc);
+      console.log(result);
+      res.json({ original_url: url, short_url: count });
+    }
+  );
 });
 
 app.get('/api/shorturl/:shorturl', async (req, res) => {
   const shorturl = parseInt(req.params.short_url);
-  const urlDoc = await urls.findOne({ short_url: +shorturl })
+  const urlDoc = await urls.findOne({ short_url: +shorturl });
 
   if (!urlDoc) {
-    return res.json({ error: "Short URL not found" });
+    return res.json({ error: 'Short URL not found' });
   }
 
   res.redirect(urlDoc.url);
 });
 
-app.listen(port, function() {
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
